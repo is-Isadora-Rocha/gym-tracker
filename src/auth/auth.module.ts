e568734f8b48a0fe1330cfa8b9897a83cf27e43c
is-Isadora-Rocha <is.isadora.rocha@gmail.com>
2024-11-26 16:42:51 -0300

    JWT secret

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,8 +1,17 @@
 import { Module } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
+import { JwtModule } from '@nestjs/jwt';
 
 @Module({
+  imports: [
+    JwtModule.registerAsync({
+      useFactory: async () => ({
+        secret: process.env.JWT_SECRET,
+        signOptions: { expiresIn: '1d'}
+      })
+    })
+  ],
   providers: [AuthService],
   controllers: [AuthController]
 })
