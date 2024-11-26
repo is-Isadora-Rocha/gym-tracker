ab2f0061777d8549f01dd40bd4d72967b9e0daf9
is-Isadora-Rocha <is.isadora.rocha@gmail.com>
2024-11-26 12:06:27 -0300

    mod ser con exercise

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { ExerciseModule } from './exercise/exercise.module';
 
 @Module({
-  imports: [AuthModule, UserModule],
+  imports: [AuthModule, UserModule, ExerciseModule],
   controllers: [AppController],
   providers: [AppService],
 })
