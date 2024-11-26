924f9f1bdb9c6ed4ec56a27ceede6209727299f5
is-Isadora-Rocha <is.isadora.rocha@gmail.com>
2024-11-26 12:08:58 -0300

    mod ser con progress

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,10 @@ import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { ExerciseModule } from './exercise/exercise.module';
 import { WorkoutModule } from './workout/workout.module';
+import { ProgressModule } from './progress/progress.module';
 
 @Module({
-  imports: [AuthModule, UserModule, ExerciseModule, WorkoutModule],
+  imports: [AuthModule, UserModule, ExerciseModule, WorkoutModule, ProgressModule],
   controllers: [AppController],
   providers: [AppService],
 })
