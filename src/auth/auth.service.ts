7bfff5540351cb9de75862845e687ceaa9e26bec
is-Isadora-Rocha <is.isadora.rocha@gmail.com>
2024-11-27 10:53:05 -0300

    validation auth

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
-    constructor(private jwtSertive: JwtService) {}
+    constructor(private jwtServive: JwtService) {}
 
-    
+    async hashPassword(password: string): Promise<string> {
+        return bcrypt.hash(password, 10);
+    }
+
+    async validatePassword(password: string, hashedPassword: string): Promise<boolean> {
+        return bcrypt.compare(password, hashedPassword);
+    }
+
+    async generateToken(user: any): Promise<string> {
+        const payload = { username: user.username, sub: user.id };
+        return this.jwtServive.sign(payload);
+    }
+
 }
