aba8921fff0e61d296cc5a268adc1db6a6515a91
is-Isadora-Rocha <is.isadora.rocha@gmail.com>
2024-11-27 10:52:52 -0300

    rotas exercise

--- src/exercise/exercise.controller.ts
+++ src/exercise/exercise.controller.ts
@@ -1,4 +1,50 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post, Put } from '@nestjs/common';
+import { ExerciseService } from './exercise.service';
 
 @Controller('exercise')
-export class ExerciseController {}
+export class ExerciseController {
+    constructor(private readonly exerciseService: ExerciseService) {}
+
+    @Post('add')
+    async createExercise(@Body() body) {
+        const { name, group } = body;
+        return this.exerciseService.createExercise(name, group);
+    }
+
+    @Get()
+    async getAllExercises() {
+        return { exercises: [] }
+    }
+
+    @Get(':id')
+    async getOneExercise(@Param() params){
+        return { exercise: [], params}
+    }
+
+    @Put(':id')
+    async updateExercise(@Body() body, @Param() params){
+        return {
+            method: 'put',
+            body, 
+            params
+        }
+    }
+
+    @Patch(':id')
+    async updatePartial(@Body() body, @Param() params){
+        return {
+            method: 'patch',
+            body, 
+            params
+        }
+    }
+    
+    @Delete(':id')
+    async delete(@Param() params) {
+        return {
+            params
+        }
+    }
+
+
+}
--- src/exercise/exercise.service.ts
+++ src/exercise/exercise.service.ts
@@ -1,4 +1,9 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class ExerciseService {}
+export class ExerciseService {
+    async createExercise(name: string, group: string) {
+        // implementar lógica de criação no banco de dados
+        return { message: `Exercise ${name} from group ${group} created successfully!` };
+    }
+}
